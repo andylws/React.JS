7796a45a5a2dadfc04b1e375d9a78b2a6df7c743
andylws <andylws@naver.com>
2021-03-06 02:51:34 +0900

    just some test

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,9 +1,14 @@
+function Player({name}) {
+  return <h1>I like {name}</h1>;
+}
 
 function App() {
   return <div>
-    <h1>
-      Hello!
-    </h1>
+    <h1>Hello!</h1>
+    <Player name="Muller" />
+    <Player name="Neuer" />
+    <Player name="Coman" />
+    <Player name="Lewandowski" />
   </div>;
 }
 
