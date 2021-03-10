8964c1d76458bda6d952381892882cf2302b917b
andylws <andylws@naver.com>
2021-03-10 15:53:45 +0900

    get some infos from API
    made Movie.js for proptypes
    get id, title, year, summary, and poster from API
    showed only titles yet

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
+import Movie from "./Movie";
 
 // ##############################################################################
-
 
 // const bayernPlayers = [
 //   {
@@ -78,9 +78,7 @@ import axios from "axios";
 //   </div>;
 // }
 
-
 // ##############################################################################
-
 
 // class App extends React.Component {
 //   constructor(props){ //JS thing, not React thing
@@ -117,24 +115,44 @@ import axios from "axios";
 //   }
 // }
 
-
 // ##############################################################################
-
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
   };
   getMovies = async () => {
-    const movies = axios.get("https://yts-proxy.now.sh/list_movies.json");
-  }
-  componentDidMount(){
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready!"}</div>
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
